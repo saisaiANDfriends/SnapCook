b19175e027e1ff1f10e68655317afc82ebcce131
saisaiANDfriends <jademagdua@gmail.com>
2026-03-06 18:49:54 +0800

    change api gemini 3.1 flash

--- crud.py
+++ crud.py
@@ -144,7 +144,7 @@ def get_favorites(user_id: str):
 # --- 5. AI: IMAGE SCAN (STRICT QUANTITIES) ---
 def analyze_image_with_gemini(image_bytes):
     print("--- [DEBUG] Sending Image to Gemini with STRICT Constraints... ---")
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    model = genai.GenerativeModel('gemini-3.1-flash-lite')
 
     # --- THE NEW HYPER-STRICT PROMPT ---
     prompt = """
@@ -231,7 +231,7 @@ async def search_recipes_by_text(ingredients_list: list):
             return doc.to_dict()
 
     # 2. ASK AI
-    model = genai.GenerativeModel('gemini-2.5-flash') 
+    model = genai.GenerativeModel('gemini-3.1-flash-lite') 
 
     prompt = f"""
     ROLE: You are an expert Chef.
